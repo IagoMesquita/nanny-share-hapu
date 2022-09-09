7a0aca0aadcd20021d4d1a969a32923a038fcc32
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-09 15:24:13 -0300

    feat: component PaymentMethodsSection

---
+++ src/components/PaymentMethodsSection/index.jsx
@@ -0,0 +1,27 @@
+import styles from "./PaymentMethodsSection.module.css";
+import valuesImage from "../../images/ImageSection3.svg";
+
+import React from "react";
+
+function PaymentMethodsSection() {
+  return (
+    <section>
+      <img src={valuesImage} alt="valores por hora" />
+      <div>
+        <h2>Shared payments made simple</h2>
+        <p>
+          Sometimes it’s hard enough just sharing a restaurant bill. Try sharing
+          that bill week in, week out and you might encounter more than a few
+          snares. But not with Hapu. Simply set your rates and our automated
+          payment system takes care of the rest. You need never talk money or
+          who owes what.
+        </p>
+				<a href="default">
+				Read how Bridget’s share (without Hapu) ended over $15
+				</a>
+      </div>
+    </section>
+  );
+}
+
+export default PaymentMethodsSection;
