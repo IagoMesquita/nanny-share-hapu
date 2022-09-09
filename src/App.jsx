db77cf32467368cd842e657da778b13519a8bd3c
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-09 15:25:03 -0300

    feat: receive componets in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import './App.css';
-import HeroSection from './components/HeroSection';
-import NotificationAvailability from './components/NotificationAvailability';
+import "./App.css";
+//components
+import HeroSection from "./components/HeroSection";
+import NotificationAvailability from "./components/NotificationAvailability";
+import IntroSection from "./components/IntroSection";
+import Footer from "./components/Footer/inex";
+import ShareSection from "./components/ShareSection";
+import PaymentMethodsSection from "./components/PaymentMethodsSection";
+import BillingHistorySection from "./components/BillingHistorySection";
+import NannyDiarySection from "./components/NannyDiarySection";
 
 function App() {
   return (
     <>
-      <HeroSection/>
-      <NotificationAvailability/>
-      <h2>Aola</h2>
-      <h2>Aola</h2>
-      <h2>Aola</h2>
-      <h2>Aola</h2>
-      <h2>Aola</h2>
+      <HeroSection />
+      <NotificationAvailability />
+      <main>
+        <IntroSection/>
+        <ShareSection/>
+        <PaymentMethodsSection/>
+        <BillingHistorySection/>
+        <NannyDiarySection/>
+      </main>
+      <Footer/>
     </>
   );
 }
