2b70eef6e6942b041613645af9ac4fdfee05ee72
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-10 14:23:36 -0300

    feat: save user data in state useState

--- src/components/ShareSection/index.jsx
+++ src/components/ShareSection/index.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import DividingLine from "../DividingLine";
 import styles from "./ShareSection.module.css";
 
 function ShareSection() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const user ={
+      name,
+      email
+    }
+    console.log(user);
+    setName('');
+    setEmail('');
+
+  };
+
   return (
     <>
       <section className={styles.section_container}>
@@ -12,9 +29,21 @@ function ShareSection() {
             becomes available in your area!
           </p>
         </div>
-        <form className={styles.form_container}>
-          <input type="text" name="name" placeholder="Your name" />
-          <input type="email" name="email" placeholder="Your email" />
+        <form onSubmit={handleSubmit} className={styles.form_container}>
+          <input
+            type="text"
+            name="name"
+            valune={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Your name"
+          />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Your email"
+          />
           <button>Send</button>
         </form>
       </section>
